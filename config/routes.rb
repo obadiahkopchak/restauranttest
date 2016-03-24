5a40e3397a38d545e557f8676760d07b0faacdfc
obadiahkopchak <okopchak@gmail.com>
2016-03-24 18:00:30 -0400

    you are just finishing up 4.1

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   resources :chirps
   resources :users
   resources :restaurants
-  get 'users#index'
+  
+  #root 'users#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
